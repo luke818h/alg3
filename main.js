edb7fe549156eb03b7187b6e92806c61cb8b27cf
Luke Henwood <luke8h@gmail.com>
2017-04-26 23:24:49 -0500

    found how to run algorithim for Palindrom

---
+++ main.js
@@ -0,0 +1,96 @@
+function palindrome(str) {
+  // Step 1. Lowercase the string and use the RegExp to remove unwanted characters from it
+  var re = /[\W_]/g; // or var re = /[^A-Za-z0-9]/g;
+
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  // str.toLowerCase() = "A man, a plan, a canal. Panama".toLowerCase() = "a man, a plan, a canal. panama"
+  // str.replace(/[\W_]/g, '') = "a man, a plan, a canal. panama".replace(/[\W_]/g, '') = "amanaplanacanalpanama"
+  // var lowRegStr = "amanaplanacanalpanama";
+
+  // Step 2. Use the same chaining methods with built-in functions from the previous article 'Three Ways to Reverse a String in JavaScript'
+  var reverseStr = lowRegStr.split('').reverse().join('');
+  // lowRegStr.split('') = "amanaplanacanalpanama".split('') = ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"]
+  // ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"].reverse() = ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"]
+  // ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"].join('') = "amanaplanacanalpanama"
+  // So, "amanaplanacanalpanama".split('').reverse().join('') = "amanaplanacanalpanama";
+  // And, var reverseStr = "amanaplanacanalpanama";
+
+  // Step 3. Check if reverseStr is strictly equals to lowRegStr and return a Boolean
+  return reverseStr === lowRegStr; // "amanaplanacanalpanama" === "amanaplanacanalpanama"? => true
+}
+console.log("A man, a plan, a canal. Panama");
+console.log(palindrome("A man, a plan, a canal. Panama"));
+
+
+
+
+
+
+
+/*
+str.replace(regexp|substr, newSubstr|function)
+console.log('ALPHABET'.toLowerCase()); // 'alphabet'
+
+To pass the last test case, we can use two Regular Expressions:
+/[^A-Za-z0–9]/g  or
+/[\W_]/g
+
+\W removes all non-alphanumeric characters:
+\W matches any non-word character
+\W is equivalent to [^A-Za-z0–9_]
+\W matches anything that is not enclosed in the brackets
+
+[^A-Z] matches anything that is not enclosed between A and Z
+[^a-z] matches anything that is not enclosed between a and z
+[^0-9] matches anything that is not enclosed between 0 and 9
+[^_] matches anything that does not enclose _
+
+------------------We finally have “/[\W_]/g”----------------global search
+
+toLowerCase()
+method to return the calling string value converted to lowercase.
+
+The replace()
+method to return a new string with some
+or all matches of a pattern replaced by a replacement.
+We will use one of the RegExp we just created earlier.
+
+The split()
+method splits a String object into an
+array of strings by separating the string into sub strings.
+
+The reverse()
+method reverses an array in place.
+The first array element becomes the last and the
+last becomes the first.
+
+The join()
+method joins all elements of an array into a string.
+
+function palindrome(str) {
+  // Step 1. Lowercase the string and use the RegExp to remove unwanted characters from it
+  var re = /[\W_]/g; // or var re = /[^A-Za-z0-9]/g;
+
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  // str.toLowerCase() = "A man, a plan, a canal. Panama".toLowerCase() = "a man, a plan, a canal. panama"
+  // str.replace(/[\W_]/g, '') = "a man, a plan, a canal. panama".replace(/[\W_]/g, '') = "amanaplanacanalpanama"
+  // var lowRegStr = "amanaplanacanalpanama";
+
+  // Step 2. Use the same chaining methods with built-in functions from the previous article 'Three Ways to Reverse a String in JavaScript'
+  var reverseStr = lowRegStr.split('').reverse().join('');
+  // lowRegStr.split('') = "amanaplanacanalpanama".split('') = ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"]
+  // ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"].reverse() = ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"]
+  // ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"].join('') = "amanaplanacanalpanama"
+  // So, "amanaplanacanalpanama".split('').reverse().join('') = "amanaplanacanalpanama";
+  // And, var reverseStr = "amanaplanacanalpanama";
+
+  // Step 3. Check if reverseStr is strictly equals to lowRegStr and return a Boolean
+  return reverseStr === lowRegStr; // "amanaplanacanalpanama" === "amanaplanacanalpanama"? => true
+}
+
+palindrome("A man, a plan, a canal. Panama");
+
+
+
+
+*/
